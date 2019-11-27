7d0780d0abe6ef96f8b76e9d2dbc97f323cfec0d
Pietari Järvi <pietari.jarvi@gmail.com>
2019-11-27 12:33:15 +0200

    Reaktiopeli alku. Ei vielä toimintoa.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <reaction-game/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ReactionGame from "@/components/ReactionGame.vue";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ReactionGame
   }
 }
 </script>
---
+++ src/components/ReactionGame.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="reactionZone" v-on:click="startTimer">
+        Reaction zone
+        <div id="timer">
+            <span id="millis">{{ milliseconds}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ReactionGame",
+        data() {
+            return {
+                clicked: false,
+                time: 0
+            }
+        },
+        methods: {
+            startTimer(){
+
+            },
+            clickGame() {
+                alert('div clicked');
+                this.clicked = true;
+            },
+        },
+        computed:{
+
+        }
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    #reactionZone {
+        width: 100%;
+        height: 35em;
+        background: cornflowerblue;
+    }
+</style>
