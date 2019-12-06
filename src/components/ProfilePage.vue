202b5b0eb2955c3028931e9b7b869d35ecc170a6
KimW <kimwi@metropolia.fi>
2019-12-06 14:48:48 +0200

    The start of the profile page

---
+++ src/components/ProfilePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="profilePage">
+        <div id="profile">{{ username }}</div>
+        <div id="scores">
+            <ul>
+                <li v-for="score in scores" v-bind:key="score.id">score</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProfilePage',
+    data() {
+      return {
+        username: "",
+        scores: []
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
